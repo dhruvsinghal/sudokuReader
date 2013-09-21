16c05de1c500f212234b0bf6061ffd67bda3d084
Dhruv Singhal <ds793@cornell.edu>
2013-09-20 23:13:49 -0400

    Deleted assignment files

--- lib/conn_comp/listIsEmpty.m
+++
@@ -1,4 +0,0 @@
-function empty = listIsEmpty(list)
-% Return 1 if a list is empty, 0 otherwise
-
-empty = list(3) == 0;
--- lib/conn_comp/stackPush.m
+++
@@ -1,4 +0,0 @@
-function stack = stackPush(stack, row, col)
-% Push an element onto the stack
-
-    stack = listInsertAtStart(stack, row, col);
